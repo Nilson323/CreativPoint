6fea8ec24ff28acc4bad4f222ace236a99945062
Nilson323 <marvin339@outlook.de>
2017-07-18 19:38:47 +0200

    Update save.php

--- save.php
+++ save.php
@@ -4,7 +4,7 @@ $myText = $_POST["mytext"];
 $myPass = $_POST["pass"];
 
 if($action = "save") {
-  $targetFolder = "I:\Users\Nilson\Documents\Informations";
+  $targetFolder = "/path/to/folder";
   file_put_contents($targetFolder."mytext.txt", $myText);
   file_put_contents($targetFolder."mytext.txt", $myPass);
 }
